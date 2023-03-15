5839dcd0864d2b699d3e7434737cbc4885f432ff
github-nik2163984 <github-user-nik2163984@maricopa.edu>
2023-03-15 14:09:28 -0700

    fib

---
+++ src/main/java/org/example/queue/Driver.java
@@ -0,0 +1,14 @@
+package org.example.queue;
+
+public class Driver {
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayListQueue<>();
+
+        queue.offer("One");
+        queue.offer("Two");
+        queue.offer("Three");
+        queue.offer("Four");
+
+        System.out.println(queue);
+    }
+}
